6bf9f946ed581a8648a69034fc771a4f4571686a
Devesh Saini <futuredevesh@gmail.com>
2015-06-11 16:21:09 +0530

    Add new tool : lists content of a path
    class listLocals provides capability of listing contents of a directory.
    Takes single argument `path`. It is a path to the directory whose contents is going
    to be returned on call to lsdir() method.
    
    method lsdir() returns a items dictionary of a directory. Takes single optional
    `path` to change the default path which was set on initialization by user.

---
+++ tools.py
@@ -0,0 +1,85 @@
+from os.path import isdir, isfile, islink, ismount
+from os.path import join
+from os import listdir
+
+
+TYPES = {isfile: "file", isdir: "dir", islink: "link", ismount: "mountpoint"}
+
+
+class listLocals(object):
+
+    """Class for listing directories
+    """
+
+    def __init__(self, path):
+
+        # local path
+        self.path = path
+
+    def fileType(self, filename):
+
+        """Return a string containing file's filetype if known, return "other"
+        otherwise.
+
+        Known Filetypes : "file", "dir", "link", "mountpoint".
+        """
+
+        # check to see if file is of listed filetype.
+        for func in TYPES.keys():
+
+            filepath = join(self.path, filename)
+
+            if func(filepath) :
+                return TYPES[func] 
+        
+        # If file is of type not listed in TYPES
+        return "other"
+
+
+    def split_file_types(self, contentList):
+
+        """contentList -> A single list containing directories, files, links and
+        mount points and other types of files.
+
+        return a Dict containing key:value pairs where keys are : "dir", "file",
+        "link", "mountpoint" and "other".
+
+        Each key corresponds to a list of items with same file type and "other"
+        contains a list of unsupported file types."""
+
+        # Dictionary containing names of different content on its own list.
+        # Supported keys are "file", "dir", "link", "mountpoint", "others".
+        contentDict = {"file": [], "dir": [], "link": [], "mountpoint": [], "other": []}
+
+        for filename in contentList:
+
+            filetype = self.fileType(filename)
+            contentDict[filetype].append(filename)
+
+        return contentDict
+
+
+    def lsdir(self, path=None):
+
+        """Return a Dict containing items in given `path` as key:value pairs where
+        keys are : "dir", "file", "link", "mountpoint" and "other".
+
+        And each key corresponds to a list of files with same file type and "other"
+        contains a list of unsupported file types.
+        """
+
+        # If new path is given
+        if path:
+            # change current path
+            self.path = path
+
+        return self.split_file_types(listdir(self.path))
+
+
+def lsdir(path):
+
+    """Return a dictionary containing files of different types in their own
+    lists.
+    """
+
+    return listLocals(path).lsdir()
