468a28b2b4cbc1a79a3058cba005bf063b21fc8b
Devesh Saini <futuredevesh@gmail.com>
2015-06-12 17:18:51 +0530

    Color pairs defined in a separate file
    imports everything from "imports" file. Currently, everything is [curses].

---
+++ colorpairs.py
@@ -0,0 +1,30 @@
+from imports import *
+
+"""module defines curses color pairs to be used by the importer program"""
+
+try:
+
+    # Start using curses colors
+    curses.start_color()
+
+
+    """Make following fg(foreground) bg(background) color pairs"""
+
+    ## Color Pairs Specially For Files
+    # BLUE & WHITE
+    curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_WHITE)
+    # WHITE & BLUE
+    curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLUE)
+
+    ## Color Pairs Specially For Directories
+    # MAGENTA & WHITE
+    curses.init_pair(3, curses.COLOR_MAGENTA, curses.COLOR_WHITE)
+    # WHITE & MAGENTA
+    curses.init_pair(4, curses.COLOR_WHITE, curses.COLOR_MAGENTA)
+
+    ## Color Pair For Author Credits
+    # CYAN & BLACK
+    curses.init_pair(5, curses.COLOR_CYAN, curses.COLOR_BLACK)
+
+except NameError:
+    raise NameError(" Colors pairs initialization before main")
