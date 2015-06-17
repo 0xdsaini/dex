46c6c0036d6d11a5e555b06cc196d89d2c860a3d
Devesh Saini <futuredevesh@gmail.com>
2015-06-17 10:41:03 +0530

    moved config() function to config.py file.
    moved screen configuration function, config() to config.py file
    since, it is also a kind of configuration.

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 
-"""defines curses configurations"""
+"""Configures curses module, defines other configurations and functions"""
 
-from colorpairs import * # import color pairs
+from constants import * # all constant configurations
+from colorpairs import * # all color pairs
 
 # Set up bold variable,
 # BOLD[False] -> curses.A_NORMAL, BOLD[True] -> curses.A_BOLD
@@ -12,3 +13,18 @@ curses.curs_set(0)
 
 # Disable echoing typed character.
 curses.noecho()
+
+
+def config(stdscr):
+
+    """Standard screen configuration.
+    Return configured screen."""
+
+    # Enable use of KEY_UP, KEY_DOWN etc.
+    stdscr.keypad(True)
+
+    # Set background color
+    stdscr.bkgd(" ", curses.color_pair(1))
+
+    # Return configured standard screen
+    return stdscr
