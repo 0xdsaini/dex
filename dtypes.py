a0dcadfcbb0d0f3f55551f0f230524acef40304c
Devesh Saini <futuredevesh@gmail.com>
2015-06-17 10:52:00 +0530

    dtypes.py : New file which defines all dex types.
    Currently, it only defines `Content` type.
    
    `Content` type : It have three properties
      1) name       -> name of Content
      2) type       -> type of Content
      3) linePrefix -> prefix to be placed when rendering line with
                       the same content.

---
+++ dtypes.py
@@ -0,0 +1,16 @@
+"""Contains all useful types and classes"""
+
+from constants import ELEMENT_PREFIX # import ELEMENT_PREFIX constant
+
+
+class Content(object):
+
+    """Contents class"""
+
+    def __init__(self, name, type):
+
+        self.name = name
+
+        self.type = type
+
+        self.linePrefix = ELEMENT_PREFIX[self.type]
